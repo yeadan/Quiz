1ecc42bc6d3f69209a196842b4abf306d6e1e097
Sergio Bosch Vega <sergio.bosch@gmail.com>
2015-06-26 20:08:47 +0200

    Marco aplicación

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials= require('express-partials');
+
 
 var routes = require('./routes/index');
 //var users = require('./routes/users'); modificacion modulo 6 1
@@ -21,6 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 app.use('/', routes);
 // app.use('/users', users); modificacion modulo 6 1
