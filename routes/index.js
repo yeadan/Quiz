776467740e03c9f25f45bfd981b948226ff59bdc
Sergio Bosch Vega <sergio.bosch@gmail.com>
2015-07-14 15:56:17 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,12 @@ router.get('/author', function(req, res) {
   res.render('author', { title: 'Author' });
 });
 
+//Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
+
 // Definición de rutas de quizes
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
-
-
-// router.get('/quizes/question', quizController.question);
-// router.get('/quizes/answer', quizController.answer);
-
 
 module.exports = router;
