dd897bc1f371cb02a7c5c5e6ade1ad95eb2d45a0
Sergio Bosch Vega <sergio.bosch@gmail.com>
2015-07-17 20:07:36 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -21,14 +21,14 @@ exports.index = function(req,res) {
     }).catch(function(error) { next(error);});
     } else {
 		models.Quiz.findAll().then(function(quizes) {
-			res.render('quizes/index.ejs', {quizes: quizes});
+			res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 		}).catch(function(error) { next(error);})}
 };
 
 //GET /quizes/:id
 exports.show = function(req,res) {
 	//models.Quiz.findById(req.params.quizId).then(function(quiz) {
-		res.render('quizes/show', {quiz: req.quiz});
+		res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 //GET quizes/:id/answer
@@ -39,21 +39,31 @@ exports.answer=function(req, res) {
 		resultado='Correcto';
 			//res.render('quizes/answer', {quiz: quiz, respuesta: 'Correcto'});
 	} 
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 //GET /quizes/new
 exports.new = function(req,res) {
 	var quiz = models.Quiz.build( //crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"});
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 //POST /quizes/create/
 exports.create = function(req,res) {
 	var quiz = models.Quiz.build( req.body.quiz );
-	//guarda en DB los campso pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-	 res.redirect('/quizes');
-	 }) //Redirección HTTP (URL relativo) lista de preguntas
+	quiz.
+	validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render("quizes/new", {quiz: quiz, errors: err.errors});
+			} else {
+				//guarda en DB los campso pregunta y respuesta de quiz
+				quiz.
+				save({fields: ["pregunta", "respuesta"]}).
+				then(function(){res.redirect('/quizes');}) //Redirección HTTP (URL relativo) lista de preguntas
+			}
+		}
+	);	
 };
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 router.get('/author', function(req, res) {
-  res.render('author', { title: 'Author' });
+  res.render('author', { title: 'Author', errors: []});
 });
 
 //Autoload de comandos con :quizId
