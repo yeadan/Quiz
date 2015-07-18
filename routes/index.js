c0386abe10f0ae919190f42004ac4062cd538da4
Sergio Bosch Vega <sergio.bosch@gmail.com>
2015-07-18 11:22:14 +0200

    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,6 @@ router.get('/quizes/new',					quizController.new);
 router.get('/quizes/:quizId(\\d+)/edit', 	quizController.edit);
 router.post('/quizes/create',				quizController.create);
 router.put('/quizes/:quizId(\\d+)', 		quizController.update);
+router.delete('/quizes/:quizId(\\d+)', 		quizController.destroy);
+
 module.exports = router;
