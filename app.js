a970a0db9198fbc15fd38145b8da0734220032f7
Sergio Bosch Vega <sergio.bosch@gmail.com>
2015-07-23 18:59:57 +0200

    Autenticación y sesión

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials= require('express-partials');
 var methodOverride = require('method-override');
+var session = require('express-session');
 
 
 var routes = require('./routes/index');
@@ -21,15 +22,26 @@ app.use(favicon(__dirname + '/public/favicon.ico')); // cambio modulo 6 1
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Quiz 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
+
+app.use(function(req, res, next) {
+    if (!req.path.match(/\/login|\/logout/)) {
+        req.session.redir = req.path;
+    }
+    res.locals.session = req.session;
+    next();
+});
+
 
 app.use('/', routes);
 // app.use('/users', users); modificacion modulo 6 1
 
 // catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
