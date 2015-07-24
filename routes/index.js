4dda5ec3cf7d32aa4c4cad6d1cd2cef06d69ea22
Sergio Bosch Vega <sergio.bosch@gmail.com>
2015-07-24 20:28:54 +0200

    Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,7 @@ router.get('/author', function(req, res) {
 
 //Autoload de comandos con :quizId
 router.param('quizId', quizController.load); //autoload :quizId
+router.param('commentId', commentController.load); //autoload commentId
 
 //Definición de rutas de sesión
 router.get('/login', 	sessionController.new);
@@ -32,7 +33,10 @@ router.post('/quizes/create',				sessionController.loginRequired, quizController
 router.put('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizController.destroy);
 
+//Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', 		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+													sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
